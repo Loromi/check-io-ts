e38bea30b3f794dd4925cf19ea114aee5fc72e9c
Loromi <loromi94@gmail.com>
2022-10-17 11:10:26 +0200

    feat: compare split pattern and file strings

--- mission/unix-match-part-2/unix-match-part-2.ts
+++ mission/unix-match-part-2/unix-match-part-2.ts
@@ -6,19 +6,31 @@ function unixMatch(filename: string, pattern: string): boolean {
     let ptrnEnd: string = pattern.split(']')[1];
     let fileBgn: string = filename.slice(0, ptrnBgn.length);
     let fileEnd: string = filename.slice(filename.length - ptrnEnd.length, filename.length);
+    let fileMid: string = filename.slice(ptrnBgn.length, filename.length - ptrnEnd.length);
+    let seqMatch: boolean = true, ptrnMatch: boolean = false;
 
-    let lenPtrn: number = ptrnValues.length, j: number = 0;
-    let seqMatch: boolean = false, ptrnMatch: boolean = true;
-
-    for (let i = 0; i < filename.length; i++) {
-
+    if (fileBgn == ptrnBgn && fileEnd == ptrnEnd) {
+        ptrnMatch = true;
+    }
+    if (ptrnValues.indexOf(fileMid[0]) > -1) {
+        if (ptrnValues[0] == "!") {
+            seqMatch = false;
+        } else {
+            seqMatch = true;
+        }
+    } else {
+        if (ptrnValues[0] == "!") {
+            seqMatch = true;
+        } else {
+            seqMatch = false;
+        }
     }
     console.log(`seqMatch: ${seqMatch}, ptrnMatch: ${ptrnMatch}`);
     return (seqMatch && ptrnMatch);
 }
 
 console.log("true = ", unixMatch('log9.txt', '*[234567890]*'));
-console.log("true = ", unixMatch('log1.txt', 'log[234567890].txt'));
+console.log("true = ", unixMatch('log1.txt', 'log[1234567890].txt'));
 console.log("true = ", unixMatch('log1.txt', 'log[!0].txt'));
 console.log("false = ", unixMatch('log1.txt', 'log[!1].txt'));
 
